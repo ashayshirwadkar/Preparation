884742fc3499ddb028c924f39d66d1628488ed68
Ashay Shirwadkar <ashayshirwadkar12@gmail.com>
2016-10-17 14:36:18 +0530

    Fixed coding sytle issues

--- buble.c
+++ buble.c
@@ -1,43 +1,43 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
 
-void bubble(int *a,int n)
+void bubble(int *a, int n)
 {
-	int i,j,k=n,temp;
+	int i, j, k = n, temp;
 	int flag = 0;
 
-	for(i=0;i<n-1;i++) {
+	for (i = 0;i < n - 1; i++) {
 		flag = 0;
-		for(j=0;j<n-i;j++) 
-			if(*(a+j)>a[j+1]) {
+		for (j = 0;j < n - i; j++) 
+			if (*(a + j) > a[j + 1]) {
 				flag = 1;
-				temp=a[j];
-				a[j]=a[j+1];
-				a[j+1]=temp;
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
 			}
-		if(flag == 0)
+		if (flag == 0)
 			break;
 		k--;
 	}
-	for(i=0;i<n;i++)
-		printf("%d\n",a[i]);
+	for (i = 0;i < n; i++)
+		printf("%d\n", a[i]);
 }
 
 int main()
 {
-	int arr[10],n,i;
+	int arr[10], n, i;
 	struct timeval before, after;
 
 	gettimeofday(&before, NULL);
 	/* arr=(int *)malloc(10*(sizeof(int))); */
 	n = 10;
-	for( i=0;i<n;i++)
+	for (i = 0; i < n; i++)
 		arr[i] = n - i;
-	bubble(arr,n);
+	bubble(arr, n);
 	gettimeofday(&after, NULL);
 	
-	printf("Time in us: %ld\n", (after.tv_sec * 1000000)+after.tv_usec - (before.tv_sec * 1000000 + before.tv_usec));
+	printf("Time in us: %ld\n", (after.tv_sec * 1000000) + after.tv_usec - (before.tv_sec * 1000000 + before.tv_usec));
 
 	// getch();
 	return 0;
--- project.c
+++ project.c
@@ -13,42 +13,42 @@ typedef struct node {
 
 
 node list[MAX];
-int tot_index=0;
+int tot_index = 0;
 int insert(char *);
 int search(char *str);
 void read();
 void install_package(int);
-void join (int,int);
+void join (int, int);
 void show_package();
 void main()
 {
         FILE *fp;
-        int n,i,e,no1,no2;
-        char str1[10],str2[10],*p;
-        fp=fopen("ip.txt","r");
-        while(!feof(fp)) {
-                fscanf(fp,"%s%s",str1,str2);
-                no1=insert(str1);
-                p=strtok(str2,",");
-                no2=insert(p);
-                join(no1,no2);
-                while(p!=NULL) {                     
-                        p=strtok(NULL,",");
-                        if(p!=NULL){
-                                no2=insert(p);
-                                join(no1,no2);
+        int n, i, e, no1, no2;
+        char str1[10], str2[10], *p;
+        fp = fopen("ip.txt", "r");
+        while (! feof(fp)) {
+                fscanf(fp, "%s%s", str1, str2);
+                no1 = insert(str1);
+                p = strtok(str2, ",");
+                no2 = insert(p);
+                join(no1, no2);
+                while (p != NULL) {                     
+                        p = strtok(NULL, ",");
+                        if (p != NULL){
+                                no2 = insert(p);
+                                join(no1, no2);
                         }
                 }
         }
         fclose(fp);
         read();
-        while(strcmp(str1,"ok")!=0) {
+        while (strcmp(str1, "ok") != 0) {
                 printf("\nEnter package ");
-                scanf("%s",str1);
+                scanf("%s", str1);
                 n=search(str1);
-                if(list[n].flag==1)  
+                if (list[n].flag == 1)  
                         printf("Package Already Installed!!\n");
-                else if(n!=-1)
+                else if (n != -1)
                         install_package(n);
                 else
                         printf("\nPackage doesnt exist..Do you want to add?");
@@ -59,11 +59,11 @@ void main()
 int search(char *str)
 {
         int i=0;
-        if(tot_index==0)
+        if (tot_index == 0)
                 return -1;
-        while(strcmp(list[i].str,str)!=0 && i<=tot_index) 
+        while (strcmp(list[i].str, str) != 0 && i <= tot_index) 
                 i++;
-        if(i<=tot_index)
+        if (i <= tot_index)
                 return i;
         return -1;
 }
@@ -72,21 +72,21 @@ void show_package()
 {
         node *p;
         int i;
-        for(i=0;i<tot_index;i++) {
-                p=&list[i];
-                printf("%s %d",p->str,p->flag);
+        for (i = 0;i < tot_index; i++) {
+                p = &list[i];
+                printf("%s %d", p->str, p->flag);
         }
 }
 
 void read()
 {
-        int i,j=0;
+        int i, j = 0;
         node *q;
-        for(i=0;i<tot_index;i++) {
+        for (i = 0;i < tot_index; i++) {
                 j=0; 
-                printf("\n%s->",list[i].str);
-                while(j<list[i].link_no){
-                        printf(" %s ",list[i].link[j]->str);
+                printf("\n%s->", list[i].str);
+                while (j < list[i].link_no){
+                        printf(" %s ", list[i].link[j]->str);
                         j++;
                 }
                 printf("\n");
@@ -98,11 +98,11 @@ void install_package(int pack_no)
 {
         node *p;
         int i=0;
-        p=&list[pack_no];
-        p->flag=1;
-        while(p->link[i]!=NULL) {
-                pack_no=search(p->link[i]->str);
-                if(list[pack_no].flag==0)
+        p = &list[pack_no];
+        p->flag = 1;
+        while (p->link[i] != NULL) {
+                pack_no = search(p->link[i]->str);
+                if (list[pack_no].flag == 0)
                         install_package(pack_no);
                 i++;
         }
@@ -111,22 +111,22 @@ void install_package(int pack_no)
 
 void join(int no1,int no2)
 {
-        list[no1].link[list[no1].link_no]=&list[no2];
-        list[no1].link_no=  list[no1].link_no + 1;
-        list[no1].link[list[no1].link_no]=NULL;
+        list[no1].link[list[no1].link_no] = &list[no2];
+        list[no1].link_no = list[no1].link_no + 1;
+        list[no1].link[list[no1].link_no] = NULL;
 }
 
 int insert(char *str1)
 {
         int index=0;
-        index=search(str1);
-        if(index==-1) {
-                list[tot_index].flag=0;
-                list[tot_index].link_no=0;
-                list[tot_index].link[list[tot_index].link_no]=NULL;
+        index = search(str1);
+        if (index == -1) {
+                list[tot_index].flag = 0;
+                list[tot_index].link_no = 0;
+                list[tot_index].link[list[tot_index].link_no] = NULL;
                 strcpy(list[tot_index].str,str1);
                 tot_index++;
-                return tot_index-1;
+                return tot_index - 1;
         }
         else
                 return index;
